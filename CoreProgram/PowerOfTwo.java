db41be7e9b0988d18b0eb6f63412f888fb7dde5a
unknown <altamashbagwan1@gmail.com>
2021-10-01 01:10:11 +0530

    Added: Functioning of power of two

---
+++ CoreProgram/PowerOfTwo.java
@@ -0,0 +1,24 @@
+package CoreProgram;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+	public static void main(String[] args) {
+		System.out.println("Enter a Number : ");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		scanner.close();
+		System.out.println("Power of 2^" +num+" is : "+ (int) (Math.pow(2, num)));
+		System.out.println("Printing power value " +num);
+		if (num < 31)	{
+			for (int i = 0; i <= num; i++);
+			System.out.println("Power of given number is : " + (int) Math.pow(2, 1));
+		}
+		else	{
+			System.out.println("Enter the number");
+		}
+	}
+
+}
+
+
