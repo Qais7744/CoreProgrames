6ef59a6cba1f1015199d341ab5f76ec3e3fd70b9
unknown <altamashbagwan1@gmail.com>
2021-10-01 00:51:51 +0530

    Added: Functioning of Start to flip the coin

---
+++ CoreProgram/FlipCoin.java
@@ -0,0 +1,10 @@
+package CoreProgram;
+
+import java.util.*;
+
+public class FlipCoin {
+	public static void main(String[] args) {
+			System.out.println("Start to flip the coin");
+		}
+}
+
---
+++ module-info.java
@@ -0,0 +1,2 @@
+module CoreJavaProgrames {
+}
