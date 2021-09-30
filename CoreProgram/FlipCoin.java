7e03d3e77c99b14cb142bc14531cdf18640a94a2
unknown <altamashbagwan1@gmail.com>
2021-10-01 00:57:33 +0530

    Added: Functioning Calculate percentage of FlipCoin

--- CoreProgram/FlipCoin.java
+++ CoreProgram/FlipCoin.java
@@ -4,7 +4,26 @@ import java.util.*;
 
 public class FlipCoin {
 	public static void main(String[] args) {
-			System.out.println("Start to flip the coin");
-		}
+				int value;
+				int headcount = 0;
+				int tailcount = 0;
+				double heads, tails;
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter input number of times to flip the coin : ");
+				value = scanner.nextInt();
+				scanner.close();
+				for (int i = 0; i < value; i++) {
+					double random = Math.random();
+					if (random > 0.5)
+						headcount++;
+					else
+						tailcount++;
+				}
+				heads = Math.floor( headcount / (double) value * 100);
+				tails = Math.floor( tailcount / (double) value * 100);
+				System.out.println("Percentage of heads: " + heads + "%");
+				System.out.println("Percentage of tails: " + tails + "%");
+			}
 }
+
 
