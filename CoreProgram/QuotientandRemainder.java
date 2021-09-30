1cae3929cb13030c415861fe718a350cab685b6d
unknown <altamashbagwan1@gmail.com>
2021-10-01 01:17:58 +0530

    Added: Functioning of Quotient and Remainder

---
+++ CoreProgram/QuotientandRemainder.java
@@ -0,0 +1,16 @@
+package CoreProgram;
+
+public class QuotientandRemainder {
+	public static void main(String[] args) {
+
+	    int dividend = 20;
+	    int divisor = 4;
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+}
+
+
